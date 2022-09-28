f3679b139e898bc45f5e9f63bdd6bf842a2c9924
theanh <tranluutheanh17601@gmail.com>
2022-09-28 13:08:47 +0700

    update

--- Routes/route.js
+++ Routes/route.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const Controller = require('../Controllers/controller')
-const router = express.Router()
+const Controller = require("../Controllers/controller");
+const router = express.Router();
 
-router.get('/', Controller.GetAll)
-router.get('/19110387/:id', Controller.GetOne)
-router.post('/19110387/:id', Controller.Create)
-router.get('/message', Controller.GetAllMessage)
-router.get('/message/:id', Controller.GetMessage)
+router.get("/", Controller.GetAll);
+router.get("/19110324/:id", Controller.GetOne);
+router.post("/19110324/:id", Controller.Create);
+router.get("/message", Controller.GetAllMessage);
+router.get("/message/:id", Controller.GetMessage);
 
 module.exports = router;
